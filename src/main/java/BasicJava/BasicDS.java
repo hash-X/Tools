807be7c5832e8a0eafe5a92cc4c60974521be0f7
mingleizhang <mingleizhang@Ctrip.com>
2016-01-29 10:11:03 +0800

    First commit

---
+++ src/main/java/BasicJava/BasicDS.java
@@ -0,0 +1,22 @@
+package BasicJava;
+
+/**
+ * Created by root on 16-1-29.
+ */
+public class BasicDS {
+    public static void main(String[] args) {
+        int i =1; //Integer
+        double d = 1.0; //Double
+        boolean b = false; //Boolean
+        char c = 'a'; //Character
+        long l = 1L; //Long
+        byte b_ = (byte)1; //Byte
+        short s = (short)1; // Short
+
+        Integer it = Integer.valueOf(i);
+        i = it.intValue();
+
+        Integer integer = 1;
+        i = integer;
+    }
+}
---
+++ src/main/java/BasicJava/StackOverFlowError.java
@@ -0,0 +1,11 @@
+package BasicJava;
+
+public class StackOverFlowError {
+
+    static {
+        byte[] b1 = new byte[2147483647];
+    }
+    public static void main(String[] args) {
+
+    }
+}
