f34c318d142da949254879ca557270169c4744cf
mingleizhang <mingleizhang@Ctrip.com>
2016-02-17 15:44:45 +0800

    move javaproject to Tools

---
+++ src/main/java/BasicJava/Serialization/Test1.java
@@ -0,0 +1,28 @@
+package BasicJava.Serialization;
+
+/**
+ * Created by root on 16-2-17.
+ */
+import java.io.FileOutputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.Date;
+
+/**
+ * Writing to an Object Stream
+ * At first, you should get the output stream, and then write to this outputstream is ok.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        try {
+            FileOutputStream f = new FileOutputStream("tmp");
+            ObjectOutput s = new ObjectOutputStream(f);
+            s.writeObject("HelloWorld");
+            s.writeObject(new Date());
+            s.flush();
+        } catch (Exception e) {
+            e.getMessage();
+        }
+
+    }
+}
---
+++ src/main/java/scala/BasicGrammer.scala
@@ -0,0 +1,8 @@
+package scala
+
+/**
+  * Created by root on 16-2-17.
+  */
+class BasicGrammer {
+
+}
